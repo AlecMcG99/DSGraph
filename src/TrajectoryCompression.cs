15b9f66109cf73f847e6203c8dbe32d4a85d793a
Daniel Madison <daniel.madison@drake.edu>
2021-03-31 10:45:46 -0500

    More work done on Trajectory Compression class

--- src/TrajectoryCompression.cs
+++ src/TrajectoryCompression.cs
@@ -9,5 +9,16 @@ namespace WindowsFormsApp1
     public class TrajectoryCompression
     {
         public string trajectoryString { get; set; }
+        public double[] xArray { get; set; }
+        public double[] yArray { get; set; }
+        ///public int precision { get; set; }
+        public string compressedString { get; set; }
+
+        public TrajectoryCompression(int[] xarr, int[] yarr)
+        {
+            ///Need to initialize this.arrays here
+        }
+        ///Need a create traj string method and a compress method
+
     }
 }
