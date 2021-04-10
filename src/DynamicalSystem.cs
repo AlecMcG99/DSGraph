48cd466a996d4492596a0b2fb89c57dfd45f586a
AlecMcG99 <53317718+AlecMcG99@users.noreply.github.com>
2021-04-10 12:14:29 -0500

    cleaned up imports

--- src/DynamicalSystem.cs
+++ src/DynamicalSystem.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Schema;
 using org.mariuszgromada.math.mxparser;
 using ScottPlot;
 
